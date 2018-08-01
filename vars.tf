1d0a509fb799b9b6a82be0ae06350262ea72e880
Cristobal, Jose Manuel <josemanuel.cristobal@adidas-group.com>
2018-08-01 21:29:55 +0200

    keys moved to keys folder

--- vars.tf
+++ vars.tf
@@ -2,10 +2,10 @@ variable "AWS_REGION" {
   default = "eu-west-2"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "keys/mykey"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "keys/mykey.pub"
 }
 variable "AMI" {
   default = "ami-b8956b53"
