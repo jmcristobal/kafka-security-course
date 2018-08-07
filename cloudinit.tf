e3f89caadce33dc47b3df7029678b24128e0fa69
Cristobal, Jose Manuel <josemanuel.cristobal@adidas-group.com>
2018-08-07 09:26:53 +0200

    Cloudinit without file template

--- cloudinit.tf
+++ cloudinit.tf
@@ -1,11 +1,17 @@
-data "template_file" "init-script" {
-  template = "${file("cloudinit/init.cfg")}"
-}
-
 data "template_cloudinit_config" "cloud-init" {
-  "part" {
+
+  gzip = false
+  base64_encode = false
+
+  part {
     filename = "init.cfg"
     content_type = "text/cloud-config"
-    content = "${data.template_file.init-script.rendered}"
+    content = "${file("cloudinit/init.cfg")}"
   }
+
+  part {
+    content_type = "text/x-shellscript"
+    content = "${file("cloudinit/install.sh")}"
+  }
+
 }
