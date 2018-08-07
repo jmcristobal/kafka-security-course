cdd8de06c9b33111dd5a40a3a3f1217ce97921f8
Cristobal, Jose Manuel <josemanuel.cristobal@adidas-group.com>
2018-08-07 20:22:19 +0200

    Security group updated to include zookeeper and kafka ports

--- securitygroup.tf
+++ securitygroup.tf
@@ -13,6 +13,23 @@ resource "aws_security_group" "allow-ssh" {
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access"
+  }
+
+  ingress {
+    from_port = 2181
+    protocol = "tcp"
+    to_port = 2181
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Zookeeper access"
+  }
+
+  ingress {
+    from_port = 9092
+    protocol = "tcp"
+    to_port = 9092
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Kafka PLAINTEXT access"
   }
   tags {
     Name = "allow-ssh"
