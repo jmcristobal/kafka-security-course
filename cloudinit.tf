1d043f21089ea9cd1bb98f3d00fbed8c6fad5395
Cristobal, Jose Manuel <josemanuel.cristobal@adidas-group.com>
2018-08-07 20:22:02 +0200

    Initial setup of kafka server

--- cloudinit.tf
+++ cloudinit.tf
@@ -10,6 +10,7 @@ data "template_cloudinit_config" "cloud-init" {
   }
 
   part {
+    filename = "install.sh"
     content_type = "text/x-shellscript"
     content = "${file("cloudinit/install.sh")}"
   }
--- cloudinit/install.sh
+++ cloudinit/install.sh
@@ -42,6 +42,84 @@ ExecStop=/home/ubuntu/kafka/bin/kafka-server-stop.sh
 WantedBy=multi-user.target
 EOF
 
+tee <<EOF /home/ubuntu/kafka/config/server.properties >/dev/null
+############################# Server Basics #############################
+
+# The id of the broker. This must be set to a unique integer for each broker.
+broker.id=0
+
+############################# Socket Server Settings #############################
+
+# The address the socket server listens on. It will get the value returned from
+# java.net.InetAddress.getCanonicalHostName() if not configured.
+#   FORMAT:
+#     listeners = listener_name://host_name:port
+#   EXAMPLE:
+#     listeners = PLAINTEXT://your.host.name:9092
+advertised.listeners=PLAINTEXT://##your-public-DNS##:9092
+zookeeper.connect=##your-public-DNS##:2181
+
+
+# Maps listener names to security protocols, the default is for them to be the same. See the config documentation for more details
+#listener.security.protocol.map=PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL
+
+# The number of threads that the server uses for receiving requests from the network and sending responses to the network
+num.network.threads=3
+
+# The number of threads that the server uses for processing requests, which may include disk I/O
+num.io.threads=8
+
+# The send buffer (SO_SNDBUF) used by the socket server
+socket.send.buffer.bytes=102400
+
+# The receive buffer (SO_RCVBUF) used by the socket server
+socket.receive.buffer.bytes=102400
+
+# The maximum size of a request that the socket server will accept (protection against OOM)
+socket.request.max.bytes=104857600
+
+auto.create.topics.enable=false
+
+############################# Log Basics #############################
+
+log.dirs=/home/ubuntu/kafka-logs
+
+# The default number of log partitions per topic. More partitions allow greater
+# parallelism for consumption, but this will also result in more files across
+# the brokers.
+num.partitions=1
+
+# The number of threads per data directory to be used for log recovery at startup and flushing at shutdown.
+# This value is recommended to be increased for installations with data dirs located in RAID array.
+num.recovery.threads.per.data.dir=1
+
+############################# Internal Topic Settings  #############################
+# The replication factor for the group metadata internal topics "__consumer_offsets" and "__transaction_state"
+# For anything other than development testing, a value greater than 1 is recommended for to ensure availability such as 3.
+offsets.topic.replication.factor=1
+transaction.state.log.replication.factor=1
+transaction.state.log.min.isr=1
+
+# The minimum age of a log file to be eligible for deletion due to age
+log.retention.hours=168
+
+# A size-based retention policy for logs. Segments are pruned from the log unless the remaining
+# segments drop below log.retention.bytes. Functions independently of log.retention.hours.
+#log.retention.bytes=1073741824
+
+# The maximum size of a log segment file. When this size is reached a new log segment will be created.
+log.segment.bytes=1073741824
+
+# The interval at which log segments are checked to see if they can be deleted according
+# to the retention policies
+log.retention.check.interval.ms=300000
+
+
+# Timeout in ms for connecting to zookeeper
+zookeeper.connection.timeout.ms=6000
+
+EOF
+
 systemctl enable zookeeper
 systemctl enable kafka
 
